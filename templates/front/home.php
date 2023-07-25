3732bd8e6fe41b39023a1a896338879dd2a33c42
Edjek <edjek@hotmail.fr>
2023-07-25 16:30:32 +0200

    homepage

--- templates/front/home.php
+++ templates/front/home.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 
 <body>
@@ -20,6 +21,7 @@
         }
         ?>
     </div>
+
 </body>
 
 </html>
