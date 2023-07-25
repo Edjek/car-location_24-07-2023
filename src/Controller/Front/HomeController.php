bf46ecc71a55e42f562328e4a627a1afbd866015
Edjek <edjek@hotmail.fr>
2023-07-25 11:44:10 +0200

    router

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller\Front;
 
+use \App\Model\Car;
+
 class HomeController
 {
     public function index()
     {
+        // Créer une class Car dans Model avec un namespace
+            // Method public getCars()
+        $cars = new Car();
+        $cars->getCars();
         require_once '../templates/front/home.php';
     }
 }
--- src/Core/Autoloader.php
+++ src/Core/Autoloader.php
@@ -7,7 +7,6 @@ class Autoloader
     public static function register()
     {
         spl_autoload_register(function ($class) {
-            // chercher la methode php qui va me permettre de remplacer App par src
             $classPath = str_replace('App', 'src', $class);
             $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $classPath);
             require_once '..' . DIRECTORY_SEPARATOR . $classPath . '.php';
