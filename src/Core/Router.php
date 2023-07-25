b9d61afb30b642e244d0b5e075c7268595fa4b73
Edjek <edjek@hotmail.fr>
2023-07-25 22:00:15 +0200

    database.txt

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -9,25 +9,25 @@ use App\Controller\Front\CarController;
 class Router
 {
     private array $routes; // Tableau associatif pour stocker les routes et les fonction associés
-    private $currentController; // Stock le controleur actuel pour l'action demandé
+    private $currentController; // Stock le contrôleur actuel pour l'action demandé
 
     public function __construct()
     {
-        // Ajoute des routes dans le constructeur, donc à l'initailisation de l'objet
+        // Ajoute des routes dans le constructeur, donc à l'initialisation de l'objet
         $this->add_route('/', function () {
-            $this->currentController = new HomeController(); // Créé une instance du controlleur d'accueil
-            $this->currentController->index(); // Appelle la méthode index du controleur d'accueil
+            $this->currentController = new HomeController(); // Créé une instance du contrôleur d'accueil
+            $this->currentController->index(); // Appelle la méthode index du contrôleur d'accueil
         });
-        // Route pour la page contact avec un parametre id
-        $this->add_route('/contact/test/{id}', function ($params) { // On peut passer les eventuels paramètres à la fonction
+        // Route pour la page contact avec un paramètre id
+        $this->add_route('/contact/test/{id}', function ($params) { // On peut passer les éventuels paramètres à la fonction
             $this->currentController = new ContactController();
             $this->currentController->index($params);
         });
-        $this->add_route('/contact/form/{id}', function ($param) {
+        $this->add_route('/contact', function ($param) {
             $this->currentController = new ContactController();
             $this->currentController->saveForm($param);
         });
-        $this->add_route('/car/{test}', function ($params) {
+        $this->add_route('/car', function ($params) {
             $this->currentController = new CarController();
             $this->currentController->index($params);
         });
@@ -35,22 +35,22 @@ class Router
 
     private function add_route(string $route, callable $closure)
     {
-        // Convertit la route en une expression régulière pour une correspondance flexible en url et parametre
-        $pattern = str_replace('/', '\/', $route); // Echappe les barres obliques pour la regex
-        $pattern = preg_replace('/\{(\w+)\}/', '(?P<$1>[^\/]+)',  $pattern); // Remplace les parties entre accolade par des grouoes de capture (parametre)
+        // Convertit la route en une expression régulière pour une correspondance flexible en url et paramètre
+        $pattern = str_replace('/', '\/', $route); // Échappe les barres obliques pour la regex
+        $pattern = preg_replace('/\{(\w+)\}/', '(?P<$1>[^\/]+)',  $pattern); // Remplace les parties entre accolade par des groupes de capture (conserve les paramètres)
         $pattern = '/^' . $pattern . '$/'; // Ajoute les délimiteurs de début et de fin de la regex
         $this->routes[$pattern] = $closure; // Stock la regex de la route et la fonction associée dans le tableau des routes
     }
 
     public function execute()
     {
-        $requestUri = $_SERVER['REQUEST_URI']; // Récupère l'URL de la requete
+        $requestUri = $_SERVER['REQUEST_URI']; // Récupère l'URL de la requête
         $finalPath = str_replace('/car-location', '', $requestUri); // Supprime le dossier racine pour obtenir le chemin final
 
         foreach ($this->routes as $key => $closure) {
             if (preg_match($key, $finalPath, $matches)) {
                 array_shift($matches);
-                $closure($matches); // Appelle la fonction associée à la route avec les eventuels paramètres capturés
+                $closure($matches); // Appelle la fonction associée à la route avec les éventuels paramètres capturés
                 return;
             }
         }
