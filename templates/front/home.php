2046ded855178f7bad17adc494cdfa5fec077574
Edjek <edjek@hotmail.fr>
2023-07-26 13:52:26 +0200

    home.php

--- templates/front/home.php
+++ templates/front/home.php
@@ -1,15 +1,25 @@
 <?php
 require_once '../templates/includes/header.php'
 ?>
-<section>
-    <div>
+<section class="container py-3">
+    <h1>Nos voitures</h1>
+    <div class="row flex-wrap justify-content-center">
         <?php
         foreach ($cars as $car) {
         ?>
-            <h1><?= $car['name']; ?></h1>
-            <p><?= $car['description']; ?></p>
-            <img src="/car-location/public/img/<?= $car['image']; ?>" alt="Une image de ">
-            <a href="/car-location/reservation/<?= $car['id']; ?>">reserver</a>
+            <div class="card m-2" style="width:18rem;">
+                <img src="/car-location/public/img/<?= $car['image']; ?>" alt="Une image de " class="card-img-top">
+                <div class="card-body">
+                    <h2 card-title><?= $car['name']; ?></h2>
+                    <p class="card-text">
+                        <?= $car['description']; ?> euros
+                    </p>
+                    <p>
+                        <?= $car['price']; ?>
+                    </p>
+                    <a href="/car-location/reservation/<?= $car['id']; ?>" class="btn btn-outline-success">reserver</a>
+                </div>
+            </div>
         <?php
         }
         ?>
